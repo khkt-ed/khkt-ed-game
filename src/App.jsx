d04778daadd2835482c546387bd6101cc377c82b
Vincent Dang <dhduong2006@gmail.com>
2023-03-06 21:44:50 +0700

    sfsdafdsf

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,29 @@ import Main from './pages/Main';
 
 function App() {
   const [width, setWidth] = useState(window.innerWidth);
-  const [scale, setScale] = useState(1);
+  const [scale, setScale] = useState(0.5);
 
   useEffect(() => {
     window.addEventListener("resize", () => setWidth(window.innerWidth));
   }, []);
 
-  useEffect(() => {
-    if (width > 1500  ) {
-      setScale(1);
-    }
-    if (width <= 1500) {
-      setScale(0.9);
-    }
-    if (width <= 1200) {
-      setScale(0.8);
-    }
-    if (width <= 1024) {
-      setScale(0.7);
-    }
-    if (width <= 900) {
-      setScale(0.4);
-    }
-  }, [width]);
+  // useEffect(() => {
+  //   if (width > 1500  ) {
+  //     setScale(1);
+  //   }
+  //   if (width <= 1500) {
+  //     setScale(0.9);
+  //   }
+  //   if (width <= 1200) {
+  //     setScale(0.8);
+  //   }
+  //   if (width <= 1024) {
+  //     setScale(0.7);
+  //   }
+  //   if (width <= 900) {
+  //     setScale(0.4);
+  //   }
+  // }, [width]);
 
   return (
     <div className="App">
